5f4c8fd19c7c7b1efec80b6ca3b9f75325dd23dd
Seth L. Ortiz-Hoyos <sethl.ortiz-hoyos@mac.mynetworksettings.com>
2026-02-16 00:17:55 -0500

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/virginia-cannella-portfolio/', // Replace 'your-repo-name' with your actual GitHub repository name
-  build: {
-    outDir: 'dist',
-  },
-})
