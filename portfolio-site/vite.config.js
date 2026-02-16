2d7473d2c790b100cafc0eac02a838729acf368c
Seth L. Ortiz-Hoyos <sethl.ortiz-hoyos@mac.mynetworksettings.com>
2026-02-15 23:42:14 -0500

    updated configuration page

--- portfolio-site/vite.config.js
+++ portfolio-site/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/', // Replace 'your-repo-name' with your actual GitHub repository name
+  base: '/portfolio-site/', // Replace 'your-repo-name' with your actual GitHub repository name
   build: {
     outDir: 'dist',
   },
