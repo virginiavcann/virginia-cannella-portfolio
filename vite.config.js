64b08e1d3f32ce4247360009ad522949718bccc7
Seth L. Ortiz-Hoyos <sethl.ortiz-hoyos@mac.mynetworksettings.com>
2026-03-02 19:34:45 -0500

    removed cache

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/virginia-cannella-portfolio/', // Replace 'your-repo-name' with your actual GitHub repository name
+  base: '/', // Replace 'your-repo-name' with your actual GitHub repository name
   build: {
     outDir: 'dist',
   },
